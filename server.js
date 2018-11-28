8aded82c4b5e79ea6d3b9af73e1c3b16656e2a0d
geraldojrcg <geraldojrcg@gmail.com>
2018-11-28 15:30:51 -0300

    url pra imagens added

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const cors = require('cors');
+const path = require('path');
+
 
 //iniciando app
 const app = express();
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
 app.use(cors());
 
 //iniciando o db
 mongoose.connect('mongodb://localhost:27017/apinode', { useNewUrlParser: true });
 requireDir('./src/models/');
 
+//usando as rotas
 app.use('/api', require('./src/routes'));
+
+//rota statica para a pasta uploads
+app.use('/capes', express.static(__dirname+'/uploads/'));
+
 app.listen(3001);
   
