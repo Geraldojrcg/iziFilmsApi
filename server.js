7ecef301c34f488f51b480695cca8f9689438a07
geraldojr <geraldojrcg@gmail.com>
2018-11-30 23:52:25 -0300

    corrigindo erros

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//iniciando o db
-//mongoose.connect('mongodb://localhost:27017/apinode', { useNewUrlParser: true });
-requireDir('./src/models/');
-
 //usando as rotas
 app.use('/api', require('./src/routes'));
 
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,3 @@
-const mongoose = require('mongoose');
-
-const User = mongoose.model('User');
 
 module.exports = {
     async index(req, res){
